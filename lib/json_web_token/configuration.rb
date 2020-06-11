0b974aee44d39ac5a33dcb00b0147ec900e9a0af
Ýmir Sigurðarson <ymirs@wuxinextcode.com>
2020-06-11 14:28:52 +0000

    Remove certificates from config

--- lib/json_web_token/configuration.rb
+++ lib/json_web_token/configuration.rb
@@ -1,13 +1,12 @@
 module JsonWebToken
   class Configuration
-    attr_accessor :signature_algorithm, :default_realm_issuer, :client_id, :default_audience, :certificates
+    attr_accessor :signature_algorithm, :default_realm_issuer, :client_id, :default_audience
 
     def initialize
       @signature_algorithm = nil
       @default_realm_issuer = nil
       @client_id = nil
       @default_audience = nil
-      @certificates = nil
     end
   end
 end
